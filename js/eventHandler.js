0f4c982ac546133c2edc88c217921cac59e77fff
remzaaaa <hamzachamrouk7@gmail.com>
2026-08-08 23:26:23 +0200

    modification des fonctions de mouvement -> asynchrone

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -1,4 +1,4 @@
-import { executeMoves } from "./cubeRotation.js";
+import { executeMove } from "./cubeRotation.js";
 import { cubeMap, resetMap } from "./cubeMap.js";
 import { resetHistory, deleteGroup } from "./history.js";
 import { shuffleCube } from "./shuffle.js";
@@ -7,121 +7,119 @@ import { history, historyPanel, animationDuration } from "./main.js";
 import { createPopup } from "./popup.js";
 import { resetPosition } from "./pan.js";
 
-const btnList = document.querySelectorAll("#controls .moves button");
-btnList.forEach(element => {
-    element.addEventListener("click", () => {
+function disableBtns(list){
+    list.forEach(element => {
+        element.disabled = true;
+    });
+}
+
+function enableBtns(list){
+    list.forEach(element => {
+        element.disabled = false;
+    });
+}
+
+const movesButtons = document.querySelectorAll("#controls .moves button");
+movesButtons.forEach(element => {
+    element.addEventListener("click", async () => {
         const moveName = element.textContent;
 
-        // execute the move.
-        executeMoves(moveName, "user", cubeMap, history, historyPanel, animationDuration, true)
-
-        // Prevent move spam while an animation
-        // is currently playing.
-        btnList.forEach(el => {
-            el.disabled = true;
-            setTimeout(() => {
-                el.disabled = false;
-            }, animationDuration * 1)
-        });
-        
+        disableBtns(movesButtons)
+        await executeMove(moveName, "user", cubeMap, history, historyPanel, animationDuration, true)
+        enableBtns(movesButtons) 
     });
 });
 
 const resetEL = document.querySelector("#commands .reset")
 resetEL.addEventListener("click", () => {
-    // Restore the cube to its solved state
-    // and clear the move history.
     resetMap();
     resetHistory(history, historyPanel);
 });
 
 const shuffleEl = document.querySelector("#commands .shuffle")
 shuffleEl.addEventListener("click", async () => {
-    // Perform a random shuffle sequence.
+    disableBtns(movesButtons)
     await shuffleCube();
+    enableBtns(movesButtons) 
 });
 
-let resolveMode = false;
-
 const resolveBtn = document.querySelector("#commands .resolve")
-const toolsEl = document.querySelector("#controls .tools")
+const toolsContainer = document.querySelector("#controls .tools")
+let toolMode = false;
 resolveBtn.addEventListener("click", () => {
-    resolveMode = !resolveMode;
-    document.querySelector("#controls .moves").style.display = resolveMode ? "none" : "flex";
-    toolsEl.style.display = resolveMode ? "flex" : "none";
+    toolMode = !toolMode; // toggle on/off toolMode.
+
+    document.querySelector("#controls .moves")
+    .style.display = toolMode ? "none" : "flex";
+
+    toolsContainer
+    .style.display = toolMode ? "flex" : "none";
+
     resolveBtn.classList.toggle("active")
 });
 
-// 
+// colorPicker
 
-const picker = document.querySelector(".paintCube");
-const preview = picker.querySelectorAll("span");
-const input = picker.querySelectorAll("input");
+const colorsContainer = document.querySelector(".paintCube");
+const colorPreviews = colorsContainer.querySelectorAll("span");
 
-preview.forEach(element => {
-    const inputColor = element.querySelector("input");
+colorPreviews.forEach(preview => {
+    const colorPickerInput = preview.querySelector("input");
 
-    element.addEventListener("click", () => {
-        // picker.style.backgroundColor = inputColor.value;
-        picker.style.backgroundColor = inputColor.value + "99";
+    preview.addEventListener("click", () => {
+        colorsContainer.style.backgroundColor = 
+        colorPickerInput.value + "33";
+        // opacity ~ 0.2 (33 for hex)
     });
 
-    // PC (right click)
-    element.addEventListener("contextmenu", (e) => {
+    // right click
+    preview.addEventListener("contextmenu", (e) => {
         e.preventDefault();
-        inputColor.showPicker();
+        colorPickerInput.showPicker();
     });
 
-    // Mobile (long press)
     let timer;
-
-    element.addEventListener("pointerdown", () => {
+    preview.addEventListener("pointerdown", () => {
         timer = setTimeout(() => {
-            inputColor.showPicker();
-        }, 600);
+            colorPickerInput.showPicker();
+        }, 600); // long press for mobile user
     });
 
-    element.addEventListener("pointerup", () => {
+    preview.addEventListener("pointerup", () => {
         clearTimeout(timer);
     });
-
-    element.addEventListener("pointerleave", () => {
+    preview.addEventListener("pointerleave", () => {
         clearTimeout(timer);
     });
 });
 
-input.forEach(element => {
+const colorPickerInputs = colorsContainer.querySelectorAll("input");
+colorPickerInputs.forEach(element => {
     element.addEventListener("change", () => {
         updateColor();
-        picker.style.backgroundColor = element.value + "99";
+        colorsContainer.style.backgroundColor = 
+        element.value + "33";
     });
 });
 
 const resetColorEl = document.querySelector(".resetColor button")
 resetColorEl.addEventListener("click", () => {
-    // Restore the default cube color scheme.
     resetColor();
 });
 
 const resetPosEl = document.querySelector(".resetPos button")
 resetPosEl.addEventListener("click", () => {
-    // Restore the default cube position.
     resetPosition(animationDuration);
 });
 
 const debugInput = document.querySelector(".debug input");
 const mainCube = document.getElementById("mainCube");
 debugInput.addEventListener("click", () => {
-    // Debug mode displays cube IDs
-    // directly on the cube.
-    if (debugInput.checked){
-        mainCube.classList.add("showCell");
-    } else{
-        mainCube.classList.remove("showCell");
-    }
+    if (debugInput.checked) mainCube.classList.add("showCell");
+    else mainCube.classList.remove("showCell");
 })
 
-// 
+// popup
 
 const popup = document.getElementById("popup");
 let selectedGroup = null;
@@ -145,15 +143,47 @@ historyPanel.addEventListener("click", (event) => {
 });
 
 
-popup.addEventListener("click", (e) => {
+popup.addEventListener("click", async (e) => {
     const button = e.target.closest("button");
 
     if (!button) return;
 
     if (button.textContent === "yes") {
-        deleteGroup(history, historyPanel, parseInt(selectedGroup.id));
+        await deleteGroup(history, historyPanel, parseInt(selectedGroup.id));
     }
 
     popup.style.display = "none";
+});
+
+// playbackControls
+
+const previousBtn = document.querySelector(".previous");
+const pauseBtn = document.querySelector(".pause");
+const playBtn = document.querySelector(".play");
+const stopBtn = document.querySelector(".stop");
+const nextBtn = document.querySelector(".next");
+
+const playbackControls = document.querySelector(".playbackControls")
+playbackControls.querySelectorAll("button").forEach(element => {
+    element.addEventListener("click", async () => {
+        const btnName = element.classList[0];
+        switch (btnName) {
+            case "previous":
+                
+                break;
+            case "pause":
+                
+                break;
+            case "play":
+                
+                break;
+            case "stop":
+                
+                break;
+            case "next":
+                
+                break;
+        }
+    })
 });
 
