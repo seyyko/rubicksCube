14a5c26db82e8626dfa44c5e144a72ff2b973b35
remzaaaa <hamzachamrouk7@gmail.com>
2026-08-18 18:05:19 +0200

    modif des commentaires

--- js/algoFinalMove.js
+++ js/algoFinalMove.js
@@ -34,45 +34,45 @@ export async function finalMove(map, history, panel=null, animationDuration=0, c
 
     corners = cornersCube.map(cube => isCornerPlaced(map, cube, 5, facesColors.downCenter))
     count = corners.filter(Boolean).length;
-    console.log("#finalMoveSet#: corners list:", corners);
+    console.log("#finalMoveSet# corners list:", corners);
 
     while (count < 4){
-        console.log("#finalMoveSet#: how many corners are in good position ?", count)
-        console.log("#finalMoveSet#: is the current corner is in good position ?", corners[0])
+        console.log("#finalMoveSet# how many corners are in good position ?", count)
+        console.log("#finalMoveSet# is the current corner is in good position ?", corners[0])
         if (!corners[0]){
-            console.log("#finalMoveSet#: the current corner isn't in a good position.");
+            console.log("#finalMoveSet# the current corner isn't in a good position.");
             if (getAlgoDirection(map,facesColors.downCenter)){
-                console.log("#finalMoveSet#: sticker that contain the down face color is on the right.");
-                console.log("#finalMoveSet#: the algorithm is 2 Sexy move (R U R' U').");
+                console.log("#finalMoveSet# sticker that contain the down face color is on the right.");
+                console.log("#finalMoveSet# the algorithm is 2 Sexy move (R U R' U').");
                 move = ["R", "U", "R'", "U'", "R", "U", "R'", "U'"];
             }else{
-                console.log("#finalMoveSet#: sticker that contain the down face color is on the left.");
-                console.log("#finalMoveSet#: the algorithm is 2 reversed Sexy move (U R U' R').");
+                console.log("#finalMoveSet# sticker that contain the down face color is on the left.");
+                console.log("#finalMoveSet# the algorithm is 2 reversed Sexy move (U R U' R').");
                 move = ["U", "R", "U'", "R'", "U", "R", "U'", "R'"];
             }
             await executeMoves(move, "solver", map, history, panel, animationDuration, changeBg);
         }
-        console.log("#finalMoveSet#: do D' then, we check the next corner.");
+        console.log("#finalMoveSet# do D' then, we check the next corner.");
         move = "D'";
         await executeMove(move, "solver", map, history, panel, animationDuration, changeBg);
         
         facesColors = getFacesColors(map);
         corners = cornersCube.map(cube => isCornerPlaced(map, cube, 5, facesColors.downCenter))
         count = corners.filter(Boolean).length;
-        console.log("#finalMoveSet#: corners list:", corners);
+        console.log("#finalMoveSet# corners list:", corners);
     }
 
-    console.log("#finalMoveSet#: we finish by turning the down face until the cube is done.");
+    console.log("#finalMoveSet# we finish by turning the down face until the cube is done.");
 
     while (
         !(facesColors.frontBotCenter === facesColors.frontCenter)
     )
     {
-        console.log("#finalMoveSet#: down face aligned ?", facesColors.frontBotCenter === facesColors.frontCenter);
+        console.log("#finalMoveSet# down face aligned ?", facesColors.frontBotCenter === facesColors.frontCenter);
         move = "D"
         await executeMove(move, "solver", map, history, panel, animationDuration, changeBg);
         facesColors = getFacesColors(map);
     }
     
-    console.log("#finalMoveSet#: cube is resolved !!!");
+    console.log("#finalMoveSet# cube is resolved !!!");
 }
