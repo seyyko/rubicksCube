84da3fe70da546414f8cdcf79542371cd22ab4fe
remzaaaa <hamzachamrouk7@gmail.com>
2026-08-23 17:08:09 +0200

    add an Id to the popup, ability to remove the desc part, adding the btns part to be able to create only 1 button.

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,26 @@
-export function createPopup(title, id, desc){
+export function createPopup(popupId, title, id, desc, btns){
     const popup = document.getElementById("popup");
+    popup.classList.remove(...popup.classList);
+    popup.classList.add(popupId);
     popup.querySelector(".title").textContent = title
     popup.querySelector(".id p:nth-of-type(1)").textContent = id[0]
     popup.querySelector(".id p:nth-of-type(2)").textContent = id[1]
-    popup.querySelector(".desc p:nth-of-type(1)").textContent = desc[0]
-    popup.querySelector(".desc p:nth-of-type(2)").textContent = desc[1]
+    if (desc === null){
+        const desc = popup.querySelector(".desc");
+        popup.classList.add("noDesc");
+    }else{
+        popup.querySelector(".desc p:nth-of-type(1)").textContent = desc[0]
+        popup.querySelector(".desc p:nth-of-type(2)").textContent = desc[1]
+    }
+    if (btns){
+        for (let i = 0; i < btns.length; i++) {
+            const btn = popup.querySelector(`button:nth-of-type(${i+1})`);
+            if (btns[i] === ""){
+                btn.style.display = "none";
+                popup.classList.add("singleBtn");
+                continue;
+            }
+            btn.querySelector("b").textContent = btns[i]  
+        }
+    }
 }
